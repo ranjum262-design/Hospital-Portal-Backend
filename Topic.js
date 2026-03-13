ba03f6014ac90c0fea50cb87fd057e7b0f3d91e2
ranjum262-design <ranjum262@gmail.com>
2026-03-13 16:43:54 +0530

    Update Topic.js

--- Topic.js
+++ Topic.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Topic = require('./Topic');
+
 
 const TopicSchema = new mongoose.Schema({
   title: { type: String, required: true },
@@ -14,3 +14,4 @@ const TopicSchema = new mongoose.Schema({
 
 
 module.exports = mongoose.model('Topic', TopicSchema, 'hospital_portal');
+
