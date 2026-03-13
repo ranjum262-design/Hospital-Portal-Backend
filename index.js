370d90daa4632572d416b1266e16177a18add4db
ranjum262-design <ranjum262@gmail.com>
2026-03-13 16:46:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const userRoutes = require('./userRoutes');
 app.use('/api/users', userRoutes); 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`🚀 Server running on port ${PORT}`));
+
 
 
