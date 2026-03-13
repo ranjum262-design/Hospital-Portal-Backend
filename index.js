cde036372021f410650eca336e8ea9e7740a029e
ranjum262-design <ranjum262@gmail.com>
2026-03-13 20:25:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 // 1. Connect Database
 connectDB();
 app.use(cors({
-  origin: 'http://localhost:4200',
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
@@ -22,6 +22,7 @@ app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => console.log(`🚀 Server running on port ${PORT}`));
+
 
 
 
