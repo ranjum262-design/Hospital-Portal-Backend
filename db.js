a58f827181bb45b6628eec81ff02955535792f32
ranjum262-design <ranjum262@gmail.com>
2026-03-13 13:39:45 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
+// Hum process.env.MONGO_URI use karenge, agar nahi mila toh fallback ke liye string use karenge
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://ranjum262_db_user:rifatAnjum@cluster0.teea9ao.mongodb.net/hospital_portal?retryWrites=true&w=majority&appName=Cluster0');
-        console.log("✅ MongoDB Connected...");
+    const dbURI = process.env.MONGO_URI || 'mongodb+srv://ranjum262_db_user:rifatAnjum@cluster0.teea9ao.mongodb.net/hospital_portal?retryWrites=true&w=majority&appName=Cluster0';
+    await mongoose.connect(dbURI);
+    console.log("✅ MongoDB Connected...");
   } catch (err) {
     console.error("❌ Database Connection Failed", err);
     process.exit(1); 
